ba9576975e0c0e2e4ddc679d6e07fa05178d1bf8
yizhituyu <hoezhengwei2000@gmail.com>
2024-11-27 04:41:51 +0800

    Update generate_tables.py

--- generate_tables.py
+++ generate_tables.py
@@ -1,4 +1,3 @@
-# Data for Table 1
 table1_data = [
     ("A1", 41), ("A2", 18), ("A3", 21), ("A4", 63), ("A5", 2),
     ("A6", 53), ("A7", 5), ("A8", 57), ("A9", 60), ("A10", 93),
@@ -6,17 +5,14 @@ table1_data = [
     ("A16", 88), ("A17", 49), ("A18", 60), ("A19", 26), ("A20", 28)
 ]
 
-# Extract values for calculations
 value_map = dict(table1_data)
 
-# Table 2 data calculations
 table2_data = [
     ("Alpha", value_map["A5"] + value_map["A20"]),
-    ("Beta", value_map["A15"] // value_map["A7"]),  # Integer division
+    ("Beta", value_map["A15"] // value_map["A7"]), 
     ("Charlie", value_map["A13"] * value_map["A12"])
 ]
 
-# Generate Table 1 HTML content
 def create_table1():
     table1 = "<table><caption><strong>Table 1</strong></caption>"
     table1 += "<tr><th>Index #</th><th>Value</th></tr>"
@@ -25,7 +21,6 @@ def create_table1():
     table1 += "</table>"
     return table1
 
-# Generate Table 2 HTML content
 def create_table2():
     table2 = "<table><caption><strong>Table 2</strong></caption>"
     table2 += "<tr><th>Category</th><th>Value</th></tr>"
@@ -34,7 +29,6 @@ def create_table2():
     table2 += "</table>"
     return table2
 
-# HTML content template
 html_template = """
 <!DOCTYPE html>
 <html lang="en">
@@ -73,11 +67,8 @@ html_template = """
 </html>
 """
 
-# Combine the template with the generated tables
 html_content = html_template.format(table1=create_table1(), table2=create_table2())
 
-# Save to an HTML file
 with open("dynamic_tables.html", "w") as file:
     file.write(html_content)
 
-print("HTML file 'dynamic_tables.html' has been created. Open it in a browser to view the tables.")
